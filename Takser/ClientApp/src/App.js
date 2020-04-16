8a3352154dd9e798b799a8102d258c17e8da2323
Dor Shaar <Dor.Shaar@votiro.com>
2020-04-16 16:03:20 +0300

    Add Login page

--- Takser/ClientApp/src/App.js
+++ Takser/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { LoginPage } from './components/LoginPage';
 import { TasksGroupViewer } from './components/TasksGroupViewer';
 
 export default class App extends Component {
@@ -10,7 +11,7 @@ export default class App extends Component {
     render() {
         return (
             <Layout>
-                <Route exact path='/' component={Home} />
+                <Route exact path='/' component={LoginPage} />
                 <Route path='/tasks-groups' component={TasksGroupViewer} />
             </Layout>
         );
