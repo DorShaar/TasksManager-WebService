7044e82e3a933b5a26eba366c1c3de9dee9532a3
Dor Shaar <Dor.Shaar@votiro.com>
2020-05-15 16:15:41 +0300

    Add logs and fix url requests

--- Tasker.UI/src/common/TaskerUrls.js
+++ Tasker.UI/src/common/TaskerUrls.js
@@ -3,15 +3,15 @@
 export default class TaskerUrls extends React.Component {
 
     static getWorkTaskApiUrl() {
-        return 'http://localhost:8081/api/WorkTasks/';
+        return 'http://tasker-api:8080/api/WorkTasks/';
     }
 
     static getTasksGroupsApiUrl() {
-        return 'http://localhost:8081/api/TasksGroups/';
+        return 'http://tasker-api:8080/api/TasksGroups/';
     }
 
     static getNotesApiUrl() {
-        return 'http://localhost:8081/api/Notes/';
+        return 'http://tasker-api:8080/api/Notes/';
     }
 
     static getGroupsViewerUrl() {
--- Tasker.UI/src/components/domain/TaskerCache.js
+++ Tasker.UI/src/components/domain/TaskerCache.js
@@ -16,6 +16,8 @@ export default class TaskerCache {
     // Groups methods.
 
     async getGroups() {
+        window.console.log("Requesting all groups");
+
         if (!this.groupsCache.isSync) {
             await this.syncGroups();
         }
@@ -24,17 +26,20 @@ export default class TaskerCache {
     }
 
     async addGroup(newGroupName) {
-        await TaskerHttpRequester.putHttpRequest(this.tasksGroupsUrl, { GroupName: newGroupName })
+        window.console.log("Adding group " + newGroupName);
+        await TaskerHttpRequester.putHttpRequest(this.tasksGroupsUrl, { GroupName: newGroupName });
         this.groupsCache.isSync = false;
     }
 
     async updateGroupName(groupId, newGroupName) {
+        window.console.log("Adding group id" + groupId + " with new group name " + newGroupName);
         await TaskerHttpRequester.postHttpRequest(this.tasksGroupsUrl + groupId, { GroupName: newGroupName });
         this.groupsCache.isSync = false;
     }
 
     async deleteGroup(groupId) {
         if (window.confirm("Are you sure you want to delete group " + groupId)) {
+            window.console.log("Deleting group id" + groupId);
             await TaskerHttpRequester.deleteHttpRequest(this.tasksGroupsUrl + groupId);
             this.groupsCache.isSync = false;
         }
@@ -49,26 +54,33 @@ export default class TaskerCache {
 
     async getTasks(groupId) {
 
-        if (groupId === undefined || groupId === null)
+        if (groupId === undefined || groupId === null) {
+            window.console.log("Getting all work tasks");
             return await TaskerHttpRequester.getHttpRequest(this.workTasksUrl);
-
+        }
+            
+        window.console.log("Getting all work tasks of group " + groupId);
         return await TaskerHttpRequester.getHttpRequest(this.workTasksUrl + groupId);
     }
 
     async addTask(newTaskObject) {
+        window.console.log("Adding work task");
         await TaskerHttpRequester.putHttpRequest(this.workTasksUrl, newTaskObject);
     }
 
     async updateTask(taskId, newDescription) {
+        window.console.log("Updating work task " + taskId + " with new description " + newDescription);
         await TaskerHttpRequester.postHttpRequest(this.workTasksUrl + taskId, { description: newDescription });
     }
 
     async moveTask(taskId, destinationGroup) {
+        window.console.log("Moving work task " + taskId + " to new group " + destinationGroup);
         await TaskerHttpRequester.postHttpRequest(this.workTasksUrl + taskId, { groupName: destinationGroup });
     }
 
     async deleteTask(taskId) {
         if (window.confirm("Are you sure you want to delete task " + taskId)) {
+            window.console.log("Deleting work task " + taskId);
             await TaskerHttpRequester.deleteHttpRequest(this.workTasksUrl + taskId);
         }
     }
@@ -76,10 +88,12 @@ export default class TaskerCache {
     // Notes methods.
 
     async getGeneralNotes() {
+        window.console.log("Getting all general notes");
         return await TaskerHttpRequester.getHttpRequest(this.notesUrl);
     }
 
     async getNoteText(notePath) {
+        window.console.log("Getting note of path " + notePath);
         return await TaskerHttpRequester.getHttpTextRequest(this.notesUrl + notePath);
     }
 }
