eb5279fd766ae2c1131652bd0b71ea422cd4c8bc
Dor Shaar <Dor.Shaar@votiro.com>
2020-06-06 19:05:18 +0300

    Revert Cors configuration

--- Tasker/Startup.cs
+++ Tasker/Startup.cs
@@ -29,7 +29,7 @@ namespace Tasker
             {
                 options.AddDefaultPolicy(builder =>
                 {
-                    builder.WithOrigins("http*")
+                    builder.WithOrigins("http://localhost:3000", "https://localhost:3000")
                             .AllowAnyMethod()
                             .AllowAnyHeader()
                             .AllowCredentials();
