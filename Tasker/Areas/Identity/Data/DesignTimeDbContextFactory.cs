4e33bc5b55cdfc75c72294d7c316945a72849d2d
Dor Shaar <Dor.Shaar@votiro.com>
2020-08-15 21:33:34 +0300

    Add DesignTimeDbContextFactory abd migration

---
+++ Tasker/Areas/Identity/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Tasker.Areas.Identity.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TaskerContext>
+    {
+        public TaskerContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<TaskerContext>();
+            var connectionString = configuration.GetConnectionString("TaskerContextConnection");
+            builder.UseSqlServer(connectionString);
+            return new TaskerContext(builder.Options);
+        }
+    }
+}
